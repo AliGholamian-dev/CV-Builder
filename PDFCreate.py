1f53e3bb9f6331c460aa51f2b51848714ec23243
Ali Gholamian <alisepehr88@gmail.com>
2021-02-04 22:38:39 +0330

    Started to Use LATEX compiler -> AliceCV Finished

--- PDFCreate.py
+++ PDFCreate.py
@@ -1,10 +1,34 @@
-from AliceCV import createAliceCV
+import asyncio
 
 
-class createPDF:
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    stdout, stderr = await proc.communicate()
+
+    print(f"[{cmd!r} exited with {proc.returncode}]")
+    if stdout:
+        print(f"[stdout]\n{stdout.decode()}")
+    if stderr:
+        print(f"[stderr]\n{stderr.decode()}")
+
+
+class CreatePDF:
     def __init__(self, dictOfInformation):
         self.dictOfInformation = dictOfInformation
 
     def makeCV(self):
-        if self.dictOfInformation["Template"] == "AliceCV":
-            createAliceCV(self.dictOfInformation)
+        asyncio.run(run("initexmf --mkmaps"))
+        asyncio.run(run("initexmf --update-fndb"))
+        asyncio.run(
+            run(
+                f"pdflatex -interaction=nonstopmode -aux-directory=./templates/Tempfiles -include-directory=./templates/{self.dictOfInformation['Template']} -job-name=Resume.pdf -output-directory=./Output ./templates/{self.dictOfInformation['Template']}/template.tex"
+            )
+        )
+
+
+a = CreatePDF({"Template": "AliceCV"})
+
+a.makeCV()
