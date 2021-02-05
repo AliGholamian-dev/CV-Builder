28a67ff58594d6693f20de1290cf958969fbc0ce
Ali Gholamian <alisepehr88@gmail.com>
2021-02-05 21:02:32 +0330

    All CVS are capable of change + Github repo CV builder completed now going for GUI

--- PDFCreate.py
+++ PDFCreate.py
@@ -41,10 +41,10 @@ class CreatePDF:
         temptext = temptext.replace("rmail", self.dictOfInformation["Mail"])
         temptext = temptext.replace("raboutme", self.dictOfInformation["Aboutme"])
         temptext = temptext.replace("rotherinfo", self.dictOfInformation["Info"])
+        temptext = temptext.replace("rtitle", self.dictOfInformation["Title"])
 
         if self.dictOfInformation["Template"] == "AliceCV":
             temptext = temptext.replace("rpic", self.dictOfInformation["Picture"])
-            temptext = temptext.replace("rtitle", self.dictOfInformation["Title"])
             txt = ""
             for skills_set in self.dictOfInformation["Skills"]:
                 txt += "{" + skills_set[0] + "/" + str(skills_set[1]) + "},"
@@ -89,6 +89,23 @@ class CreatePDF:
                     + "}\n\t"
                 )
             temptext = temptext.replace("rexperience", txt[: len(txt) - 2])
+
+            txt = ""
+            for git_set in self.dictOfInformation["Gitrepos"]:
+                txt += (
+                    "\\twentyitem{"
+                    + git_set[0]
+                    + "}{\href{https://github.com/"
+                    + self.dictOfInformation["Git"]
+                    + "/"
+                    + git_set[1]
+                    + "}{"
+                    + git_set[1]
+                    + "}}{\href{"
+                    + git_set[2]
+                    + "}{ReadMe File}}{}\n\t"
+                )
+            temptext = temptext.replace("rrepos", txt[: len(txt) - 2])
 
         elif self.dictOfInformation["Template"] == "DeveloperCV":
             temptext = temptext.replace("rlastname", self.dictOfInformation["LastName"])
@@ -140,6 +157,95 @@ class CreatePDF:
                     + "}\n"
                 )
             temptext = temptext.replace("rexperience", txt[: len(txt) - 1])
+            txt = ""
+            for git_set in self.dictOfInformation["Gitrepos"]:
+                txt += (
+                    "\t \\entry\n\t\t   {"
+                    + git_set[0]
+                    + "}\n\t\t   {\href{https://github.com/"
+                    + self.dictOfInformation["Git"]
+                    + "/"
+                    + git_set[1]
+                    + "}{"
+                    + git_set[1]
+                    + "}}\n\t\t   {\href{"
+                    + git_set[2]
+                    + "}\n\t\t   {ReadMe File}}\n\t\t {}\n"
+                )
+            temptext = temptext.replace("rrepos", txt[: len(txt) - 1])
+
+        elif self.dictOfInformation["Template"] == "ModernCV":
+            temptext = temptext.replace("rpic", self.dictOfInformation["Picture"])
+            temptext = temptext.replace("rlastname", self.dictOfInformation["LastName"])
+            txt = "\\cvitem{Basic}{" + self.dictOfInformation["Basic"][0]
+            for sk in self.dictOfInformation["Basic"][1:]:
+                txt += ", " + sk
+            txt += (
+                "}\n\\cvitem{Intermediate}{" + self.dictOfInformation["Intermediate"][0]
+            )
+            for sk in self.dictOfInformation["Intermediate"][1:]:
+                txt += ", " + sk
+            txt += "}\n\\cvitem{Advanced}{" + self.dictOfInformation["Advanced"][0]
+            for sk in self.dictOfInformation["Advanced"][1:]:
+                txt += ", " + sk
+            temptext = temptext.replace("rskills", txt + "}\n")
+
+            txt = ""
+            for inter in self.dictOfInformation["Interests"]:
+                txt += "\\cvlistitem{" + inter + "}\n"
+            temptext = temptext.replace("rinterests", txt[: len(txt) - 1])
+            txt = ""
+            for ed_set in self.dictOfInformation["Education"]:
+                txt += (
+                    "\\cventry{"
+                    + ed_set[0]
+                    + "--"
+                    + ed_set[1]
+                    + "}{"
+                    + ed_set[2]
+                    + "}{"
+                    + ed_set[3]
+                    + "}{"
+                    + ed_set[4]
+                    + "}{}{}\n"
+                )
+            temptext = temptext.replace("reducation", txt[: len(txt) - 1])
+            txt = ""
+            for award_set in self.dictOfInformation["Awards"]:
+                txt += "\\cvitem{" + award_set[0] + "}{" + award_set[1] + "}\n"
+            temptext = temptext.replace("rawards", txt[: len(txt) - 1])
+
+            txt = ""
+            for ex_set in self.dictOfInformation["Experience"]:
+                txt += (
+                    "\\cventry{"
+                    + ex_set[0]
+                    + "}{"
+                    + ex_set[1]
+                    + "}{"
+                    + ex_set[2]
+                    + "}{"
+                    + ex_set[3]
+                    + "}{}{}\n"
+                )
+            temptext = temptext.replace("rexperience", txt[: len(txt) - 1])
+
+            txt = ""
+            for git_set in self.dictOfInformation["Gitrepos"]:
+                txt += (
+                    "\\cventry{"
+                    + git_set[0]
+                    + "}{\href{https://github.com/"
+                    + self.dictOfInformation["Git"]
+                    + "/"
+                    + git_set[1]
+                    + "}{"
+                    + git_set[1]
+                    + "}}{\href{"
+                    + git_set[2]
+                    + "}{ReadMe File}}{}{}{}\n"
+                )
+            temptext = temptext.replace("rrepos", txt[: len(txt) - 1])
 
         fout.write(temptext)
         # close input and output files
@@ -165,13 +271,17 @@ _dict = {
     "Title": "Developer",
     "Address": "ksjdfdkj",
     "Phone": "sdjhfdj",
-    "Git": "sdjfhdjkf",
+    "Git": "AliGholamian-dev",
     "Linkedin": "sdjkfkjdf",
     "Site": "sdjkfhdjkhf",
     "Mail": "dsklfjldf",
     "Aboutme": "sdjkfhnsjfhdshfkdjfhfhjsfjks",
-    "Skills": [["JAVA", 60], ["C++", 100]],
-    "Interests": "sdfjdfjslfjs\n\n\nsjkdhdjkshf\n\n\n\ndshgfdhsfg",
+    "Skills": [["JAVA", 5.8], ["C++", 6]],
+    "Basic": ["HTML", "CSS"],
+    "Intermediate": ["HTML", "CSS"],
+    "Advanced": ["HTML", "CSS"],
+    "Interests": "dsfdjfdskjkfslkfsk",  # ["asdd", "sffs", "sdfsfss"],
+    "Gitrepos": [["C++", "Cpp-Standard-Library", "Readme.md",]],
     "Education": [
         ["1865", "2020", "asd", "University", "Degree"],
         ["1865", "2020", "adsad", "University", "Degree"],
